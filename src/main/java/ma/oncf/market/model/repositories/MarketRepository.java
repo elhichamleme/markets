d9ff4175c4d66ea1cd786e1dc0918ddbe8469138
mac <elhaloui@yandex.com>
2019-06-24 22:12:38 +0100

    support rest repository for all entities

--- src/main/java/ma/oncf/market/model/repositories/MarketRepository.java
+++ src/main/java/ma/oncf/market/model/repositories/MarketRepository.java
@@ -2,9 +2,10 @@ package ma.oncf.market.model.repositories;
 
 import ma.oncf.market.model.entities.Market;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
-
+@RepositoryRestResource(collectionResourceRel = "markets", path = "markets")
 public interface MarketRepository extends MongoRepository<Market, String> {
 
     Optional<Market> findByMatricule(String matricule);
